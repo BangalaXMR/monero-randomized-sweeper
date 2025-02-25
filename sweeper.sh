3f658ec42c47c3f3112bb2937eb554af2deebe67
BangalaXMR <bangalaxmr@proton.me>
2025-02-25 13:32:18 +0000

    Update sweeper.sh
    starting the rpc, stopping the wallet & the rpc after the transaction is done

--- sweeper.sh
+++ sweeper.sh
@@ -15,7 +15,7 @@ while ! validate_time "$time"; do
     read -r -p "Schedule at what hour you want to transact (HH:MM), MAKE SURE THOSE NUMBERS ARE TRULY RANDOM WITH A DICE: " -i "HH:MM" time
 done
 read -r -p "Specify the address where funds will be sent: " -i "888tNkZrPN6JsEgekjMnABU4TBzc2Dt29EPAvkRxbANsAnjyPbb3iQ1YBRk1UXcdRsiKc9dhwMVgN5S9cQUiyoogDavup3H" address
-echo "open_wallet ${wallet} ${password}; sweep_all ${address}" | at ${time}
+echo "start_rpc; open_wallet ${wallet} ${password}; sweep_all ${address}; stop_wallet; stop_rpc" | at ${time}
 unset ${wallet}
 unset ${password}
 unset ${address}
